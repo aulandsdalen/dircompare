f2ba86b1d04f4f2c273871b0059d10a3fe3687c4
aulandsdalen <aulandsdalen@gmail.com>
2017-09-08 15:47:46 +0300

    initial commit

---
+++ getfilelist.rb
@@ -0,0 +1,24 @@
+require 'digest/md5'
+require 'csv'
+
+if ARGV.empty?
+	puts "Usage: getfilelist.rb <path/to/root/directory>"
+	exit 1
+end
+
+dir = ARGV[0]
+
+dirlist = []
+output = []
+
+dirlist = `find #{dir} -type file`.split("\n")
+
+dirlist.each do |file|
+	output << {:filename => file.sub(dir, ""), :size => File.size(file), :checksum => Digest::MD5.hexdigest(File.read(file))}
+end
+
+CSV.open("#{dir.tr('/ \\ :', '_')}.csv", "w") { |csv|
+	output.each { |record|
+		csv << record.values
+	}
+}
