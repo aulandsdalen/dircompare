043c3403e2cbd3cf8fff3ae9deec43a47dec0703
aulandsdalen <aulandsdalen@gmail.com>
2017-09-08 16:04:47 +0300

    add verbosity

--- getfilelist.rb
+++ getfilelist.rb
@@ -14,6 +14,7 @@ output = []
 dirlist = `find #{dir} -type file`.split("\n")
 
 dirlist.each do |file|
+	puts "Calculating md5 for #{file}"
 	output << {:filename => file.sub(dir, ""), :size => File.size(file), :checksum => Digest::MD5.hexdigest(File.read(file))}
 end
 
